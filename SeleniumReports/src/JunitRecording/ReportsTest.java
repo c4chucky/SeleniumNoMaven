2d28da9622b3841875cda54d6d850bdd65915ec8
Paolo Mondelo <charles.brown@assurity.co.nz>
2020-04-25 17:22:56 +1200

    Completed all three reports in UI script

--- SeleniumReports/src/JunitRecording/ReportsTest.java
+++ SeleniumReports/src/JunitRecording/ReportsTest.java
@@ -2,6 +2,7 @@ package JunitRecording;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.interactions.Actions;
 
 class ReportsTest {
@@ -17,26 +21,12 @@ class ReportsTest {
 	public void RunReport() throws InterruptedException {
 		
 		String path = System.getProperty("user.dir");
-		/*
-		 * String URL =
-		 * "https://s4h-qa.tauranga.govt.nz/sap/bc/ui2/flp?sap-client=400&sap-language=EN#Shell-home";
-		 * String username = "SAPPERF28@tauranga.govt.nz"; String password =
-		 * "5AP-User!";
-		 */
 		
 		System.setProperty("webdriver.chrome.driver", path+"/Resources/chromedriver.exe");
 		 WebDriver driver = new ChromeDriver();
 		 driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
 		 
-			/*
-			 * driver.get(URL); //Thread.sleep(2000);
-			 * driver.findElement(By.name("loginfmt")).sendKeys(username);
-			 * driver.findElement(By.id("idSIButton9")).click();
-			 * driver.findElement(By.name("passwd")).sendKeys(password);
-			 * driver.findElement(By.id("idSIButton9")).click();
-			 */
-		
-			navigateToReport(driver);
+		navigateToReport(driver);
 		
 	}
 	
@@ -44,32 +34,34 @@ class ReportsTest {
 		String username = "SAPPERF28@tauranga.govt.nz";
 		String password = "5AP-User!";
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		WebDriverWait wait=new WebDriverWait(driver, 20);
 		
 		driver.get("https://s4h-qa.tauranga.govt.nz/sap/bc/ui2/flp?sap-client=400&sap-language=EN#Shell-home");
-	    driver.wait();
-	    Thread.sleep(2000);
+		
+		WebElement loginField;
+		loginField= wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("loginfmt")));
+		loginField.click();
+		
 		driver.findElement(By.name("loginfmt")).sendKeys(username);
 		driver.findElement(By.id("idSIButton9")).click();
 		driver.findElement(By.name("passwd")).sendKeys(password);
-		driver.findElement(By.id("idSIButton9")).click();
-	    //driver.manage().window().setSize(new Dimension(1440, 828));
-	    {
-	      WebElement element = driver.findElement(By.id("__tile7-title-inner"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element, 0, 0).perform();
-	    }
-	    
-	    driver.findElement(By.id("sapUshellAnchorBarOverflowButton-img")).click();
-	    driver.findElement(By.cssSelector("#\\__item4-__list1-13-content .sapMSLITitleOnly")).click();
-	    driver.findElement(By.id("__tile37-subHdr-text")).click();
-	    driver.findElement(By.id("fin_acc_query_analyze_2CZBIFIGLQ002sapbi_snippet_ROOT")).click();
-	    driver.findElement(By.id("fin_acc_query_analyze_2CZBIFIGLQ002VARSCREEN_filterpanel1")).click();
-	    driver.findElement(By.id("fin_acc_query_analyze_2CZBIFIGLQ002OK-inner")).click();
+		Thread.sleep(2000);
+		WebElement nextButton;
+		nextButton= wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("idSIButton9")));
+		nextButton.click();
+		
+		nextButton= wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("idSIButton9")));
+		nextButton.click();
+		
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//div[@id='anchorNavigationBar']//ul[@role='listbox']/li[2]/div[@class='sapUshellAnchorItemInner']")).click();
+		
+		for (int i=1; i<=3; i++) {
+		driver.findElement(By.xpath("//div[@id='dashboardGroups']//div[@class='sapUshellEmptyLinksAreaPlaceHolder sapUshellLinksAreaHidden sapUshellTileContainer']//ul[@role='listbox']/li["+i+"]//div[@class='sapUiJSView sapUiView sapUiViewDisplayBlock']/div[@role='button']//span[@class='sapMGTTitle sapMText sapMTextMaxWidth sapUiSelectable']/span[@class='sapMTextLineClamp sapMTextMaxLine']")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//a[@id='homeBtn']/span[.='']")).click();
+		Thread.sleep(2000);
+		}
 	  }
 
 }
